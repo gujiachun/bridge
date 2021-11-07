5ff1f6f3843eb07fdec4ea4f83f3fe937f41aed8
gujiachun <767724839@qq.com>
2021-11-07 17:22:34 +0800

    提供bin目录

---
+++ bridge-admin/src/main/bin/startup.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+current_path=`pwd`
+case "`uname`" in
+    Linux)
+		bin_abs_path=$(readlink -f $(dirname $0))
+		;;
+	*)
+		bin_abs_path=`cd $(dirname $0); pwd`
+		;;
+esac
+base=${bin_abs_path}/..
+export LANG=en_US.UTF-8
+export BASE=$base
+
+if [ -f $base/bin/admin.pid ] ; then
+	echo "found admin.pid , Please run stop.sh first ,then startup.sh" 2>&2
+    exit 1
+fi
+
+if [ ! -d $base/logs ] ; then
+	mkdir -p $base/logs
+fi
+
+## set java path
+if [ -z "$JAVA" ] ; then
+  JAVA=$(which java)
+fi
+
+CUSTOM_JAVA="/usr/bridge/java/bin/java"
+if [ -z "$JAVA" ]; then
+  if [ -f $CUSTOM_JAVA ] ; then
+  	JAVA=$CUSTOM_JAVA
+  else
+  	echo "Cannot find a Java JDK. Please set either set JAVA or put java (>=1.5) in your PATH." 2>&2
+    exit 1
+  fi
+fi
+
+case "$#"
+in
+0 )
+  ;;
+2 )
+  if [ "$1" = "debug" ]; then
+    DEBUG_PORT=$2
+    DEBUG_SUSPEND="n"
+    JAVA_DEBUG_OPT="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=$DEBUG_PORT,server=y,suspend=$DEBUG_SUSPEND"
+  fi
+  ;;
+* )
+  echo "THE PARAMETERS MUST BE TWO OR LESS.PLEASE CHECK AGAIN."
+  exit;;
+esac
+
+JAVA_OPTS="-server -Xms2048m -Xmx2048m"
+echo $JAVA_OPTS
+echo "----------"
+
+JAVA_OPTS="$JAVA_OPTS -XX:+UseG1GC -XX:MaxGCPauseMillis=250 -XX:+UseGCOverheadLimit -XX:+ExplicitGCInvokesConcurrent "
+echo $JAVA_OPTS
+echo "----------"
+JAVA_OPTS=" $JAVA_OPTS -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Dfile.encoding=UTF-8"
+echo $JAVA_OPTS
+echo "----------"
+BRIDGE_OPTS="-DappName=bridge-admin -Ddruid.mysql.usePingMethod=false"
+
+for i in $base/lib/*;
+    do CLASSPATH=$i:"$CLASSPATH";
+done
+
+CLASSPATH="$base/conf:$CLASSPATH";
+
+echo "cd to $bin_abs_path for workaround relative path"
+cd $bin_abs_path
+
+echo CLASSPATH :$CLASSPATH
+$JAVA $JAVA_OPTS $JAVA_DEBUG_OPT $BRIDGE_OPTS -classpath .:$CLASSPATH com.rainbow.bridge.admin.BridgeAdminApplication
+
+#容器部署就没 必要产生admin.pid
+#echo $! > $base/bin/admin.pid
+# echo "cd to $current_path for continue"
+# cd $current_path
